274cd1994e5c34d675dea30f3ac14b4c83e188e7
Ndagi Stanley <ndagis@gmail.com>
2022-07-18 22:30:27 +0300

    Desktop and Mobile view - footer and header
    Using @click because to="/" messes up layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,12 +1,30 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header class="bg-white text-grey-10" bordered>
-      <q-toolbar>
+      <q-toolbar class="constraint">
+        <q-btn class="large-screen-only q-mr-sm" icon="apps" flat round dense />
+        <q-separator class="large-screen-only" vertical spaced />
         <q-toolbar-title class="text-tangerine">App</q-toolbar-title>
+        <q-btn
+          @click="goHome"
+          class="large-screen-only"
+          icon="home"
+          dense
+          flat
+          round
+        />
+        <q-btn
+          @click="goNext"
+          class="large-screen-only"
+          icon="add"
+          dense
+          flat
+          round
+        />
       </q-toolbar>
     </q-header>
 
-    <q-footer class="bg-white" bordered>
+    <q-footer class="bg-white small-screen-only" bordered>
       <q-tabs
         class="text-grey-10"
         active-color="primary"
@@ -23,9 +41,20 @@
   </q-layout>
 </template>
 
-<script setup></script>
+<script setup>
+function goHome() {
+  window.location.assign('/#/')
+}
+function goNext() {
+  window.location.assign('/#another')
+}
+</script>
 
 <style lang="sass">
+.q-toolbar
+  @media (min-width: $breakpoint-xs-max)
+    height: 60px
 .q-toolbar__title
-  text-align: center
+  @media (max-width: $breakpoint-xs-max)
+    text-align: center
 </style>
